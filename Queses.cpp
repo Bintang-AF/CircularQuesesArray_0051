881a0b8e73d656be70e23aadbc485df7b9d55aaf
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2025-06-12 10:25:41 +0700

    jika elemen yang dihapus berada di posisi terakhir array, kembali di awal

--- Queses.cpp
+++ Queses.cpp
@@ -48,5 +48,11 @@ class queues {
             FRONT = -1;
             REAR = -1;
         }
+        else {
+            if (FRONT == max - 1)
+            FRONT = 0;
+            else 
+            FRONT = FRONT + 1; 
+        }
     }
 }
